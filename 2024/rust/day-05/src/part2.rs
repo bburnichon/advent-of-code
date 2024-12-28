365d6c3ebe01b3ca31036ab4f72e537e9a15dea1
Benoît Burnichon <bburnichon@deezer.com>
2024-12-28 19:38:29 +0100

    Day-05

---
+++ 2024/rust/day-05/src/part2.rs
@@ -0,0 +1,161 @@
+use miette::miette;
+use nom::{
+    bytes::complete::tag,
+    character::{complete, complete::line_ending},
+    combinator::{all_consuming, opt},
+    multi::{many1, separated_list1},
+    sequence::{preceded, separated_pair, terminated},
+    IResult,
+};
+use std::{collections::HashMap, ops::Not};
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String> {
+    let (_, (rules, updates)) = parse(input)
+        .map_err(|err| miette!("Parse error: {}", err))?;
+
+    let rules: Rules = rules.iter().fold(
+        HashMap::new(),
+        |mut acc: HashMap<_, Vec<_>>, rule| {
+            acc.entry(rule.0).or_default().push(rule.1);
+            acc
+        },
+    );
+
+    let sum: u32 = updates
+        .iter()
+        .filter_map(|update| {
+            if !update.is_valid(&rules) {
+                let update = update.fix_ordering(&rules);
+
+                return update
+                    .0
+                    .get(update.0.len() / 2)
+                    .copied();
+            }
+            None
+        })
+        .sum();
+
+    Ok(sum.to_string())
+}
+
+type Rule = (u32, u32);
+type Rules = HashMap<u32, Vec<u32>>;
+struct Update(Vec<u32>);
+
+impl Update {
+    fn is_valid(self: &Self, rules: &Rules) -> bool {
+        let mut checked = Vec::with_capacity(self.0.len());
+        self.0.iter().all(|x| {
+            let mut valid = true;
+            if let Some(to_check) = rules.get(x) {
+                valid = to_check
+                    .iter()
+                    .all(|y| checked.contains(y).not())
+            }
+            if valid {
+                checked.push(*x);
+            }
+
+            valid
+        })
+    }
+
+    fn fix_ordering(self: &Self, rules: &Rules) -> Self {
+        if self.is_valid(rules) {
+            return Update(self.0.clone());
+        }
+
+        let mut ordered =
+            Update(Vec::with_capacity(self.0.len()));
+
+        for x in self.0.iter() {
+            for insert_index in (0..=ordered.0.len()).rev()
+            {
+                let before = &ordered.0[0..insert_index];
+                let after = &ordered.0[insert_index..];
+                let candidate =
+                    Update([before, &[*x], after].concat());
+
+                if candidate.is_valid(rules) {
+                    ordered = candidate;
+                    break;
+                }
+            }
+        }
+
+        ordered
+    }
+}
+
+fn parse(
+    input: &str,
+) -> IResult<&str, (Vec<Rule>, Vec<Update>)> {
+    let (input, rules) = many1(rule)(input)?;
+    let (input, updates) = all_consuming(preceded(
+        line_ending,
+        many1(update),
+    ))(input)?;
+
+    Ok((input, (rules, updates)))
+}
+
+fn rule(input: &str) -> IResult<&str, Rule> {
+    terminated(
+        separated_pair(
+            complete::u32,
+            tag("|"),
+            complete::u32,
+        ),
+        line_ending,
+    )(input)
+}
+
+fn update(input: &str) -> IResult<&str, Update> {
+    let (input, update) = terminated(
+        separated_list1(tag(","), complete::u32),
+        opt(line_ending),
+    )(input)?;
+
+    Ok((input, Update(update)))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47";
+        assert_eq!("123", process(input)?);
+        Ok(())
+    }
+}
